d981755b407f8e6aef48c8db211a7ef16d1fb817
stomashevsky <stomashevsky@users.noreply.github.com>
2025-12-11 19:00:23 +0100

    Add tags to Hero sections in Digital Ticketing and Age Compliance pages

--- src/pages/AgeCompliancePage.tsx
+++ src/pages/AgeCompliancePage.tsx
@@ -134,6 +134,9 @@ export default function AgeCompliancePage() {
           <div className="hidden md:flex gap-16 items-center max-w-[1280px] px-6 py-0 relative shrink-0 w-full">
             <div className="flex flex-1 flex-col gap-8 items-start relative min-w-0">
               <div className="flex flex-col gap-6 items-start relative shrink-0 w-full">
+                <p className="font-medium leading-5 text-sm text-[#737373]">
+                  Age compliance
+                </p>
                 <h1 className="font-bold leading-[48px] text-[48px] text-[#0a0a0a] tracking-[0px] whitespace-pre-wrap">
                   Age verification service for global compliance
                 </h1>
@@ -165,6 +168,9 @@ export default function AgeCompliancePage() {
           <div className="flex md:hidden flex-col gap-12 items-start justify-center max-w-[1280px] px-6 py-0 relative shrink-0 w-full">
             <div className="flex flex-col gap-6 items-start relative shrink-0 w-full">
               <div className="flex flex-col gap-4 items-start relative shrink-0 w-full">
+                <p className="font-medium leading-5 text-sm text-[#737373]">
+                  Age compliance
+                </p>
                 <h1 className="font-bold leading-9 text-[30px] text-[#0a0a0a] tracking-[0px] whitespace-pre-wrap">
                   Age verification service for global compliance
                 </h1>
